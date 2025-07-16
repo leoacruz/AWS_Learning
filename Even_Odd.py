583cf45d65669c857b76624cd0b07de6d2be9b7e
leoacruz <leo_cruz19@hotmail.com>
2025-07-16 14:12:15 -0600

    New Even odd programm

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,1 +1,8 @@
-print(Hola desde Git Bash)
+#Read the values
+num1 = int(input("Please enter a number: "))
+
+#Condition of the value
+if num1 % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
